8c3d55b054e6953702563032f568a659db0521eb
Amar Dura <dura.email@gmail.com>
2021-12-27 23:47:47 +0545

    resolved merge issue with  home.views.py

--- home/views.py
+++ home/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-<<<<<<< HEAD
     return render(request, 'home.html')
-=======
-    return HttpResponse('<b1>Hello World! Welcome to Home</b1>')
->>>>>>> sp-superuser-migrations
