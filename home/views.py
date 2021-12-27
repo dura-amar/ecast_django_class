597c93e7c978c4788959d2388438288d0d95a4b2
Amar Dura <dura.email@gmail.com>
2021-12-27 23:30:07 +0545

    Rendering html file in response

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return HttpResponse('<b1>Hello World! Welcome to Home</b1>')
+    return render(request, 'home.html')
