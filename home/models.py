fed244ce29ac545abad75e01810030eb185929a7
Amar Dura <dura.email@gmail.com>
2021-12-28 16:58:13 +0545

    Added base.html and Subjects Model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Introduction(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    college=models.CharField(max_length=100)
+    
+class Subject(models.Model):
+    title=models.CharField(max_length=100)
+    subject_code=models.IntegerField()
+    
+    
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Introduction
+from home.models import Subject
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    intros=Introduction.objects.all()
+    subs=Subject.objects.all()
+    return render(request, 'home.html',{'intros':intros,'subs':subs})
